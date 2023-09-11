f515b289caed2a72af5a786a6e8e4ff2bbb3a144
Aaqib Farooq <aaqibfarooq@gmail.com>
2023-09-11 13:07:34 +0100

    Add repo search input field

---
+++ app/controllers/repositories_controller.rb
@@ -0,0 +1,8 @@
+class RepositoriesController < ApplicationController
+
+  def index
+  end
+
+  def repo_search
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "repositories#index"
+
+  get "/repo_search", to: "repositories#repo_search"
 end
