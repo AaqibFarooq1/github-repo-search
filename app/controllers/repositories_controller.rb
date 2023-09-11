4aa158a07834bbbd2784e5884dc2c5e8b95d54ce
Aaqib Farooq <aaqibfarooq@gmail.com>
2023-09-11 19:27:33 +0100

    Add search github repo functionality

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,8 +1,15 @@
 class RepositoriesController < ApplicationController
-
-  def index
-  end
+  include HTTParty
 
   def repo_search
+    url = 'https://api.github.com/search/repositories'
+    search_term = params.require(:search)
+    query_params = { q: search_term }
+    response = HTTParty.get(url, query: query_params)
+
+    raise "Error #{response.code}: Error fetching GitHub repositories" unless response.code == 200
+
+    @repositories = JSON.parse(response.body)
+    render :index
   end
 end
